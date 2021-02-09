3e1187343422f628c79776765be4481f6f8b41cb
Ivo Woltring <ivo@ivonet.nl>
2021-02-09 18:13:50 +0100

    working with metadata

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,12 @@ RUN chmod +x /tables/aax_2_m4b.sh
 
 FROM ivonet/mediatools:latest
 
+RUN apt-get update \
+ && apt-get install --no-install-recommends -y  \
+       jq \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
 COPY --from=builder /tables /rcrack
 WORKDIR /rcrack
 
--- aax_2_m4b.sh
+++ aax_2_m4b.sh
@@ -9,14 +9,14 @@ AAX_FILE="$@"
 ##### Code ######
 
 # Die function
-die () {
-    echo >&2 "[ERROR] The job ended in error."
-    echo "[ERROR] $@"
-    exit 1
+die() {
+  echo >&2 "[ERROR] The job ended in error."
+  echo "[ERROR] $@"
+  exit 1
 }
 
 if [ -z "${AAX_FILE}" ]; then
-   die "No input aax file provided."
+  die "No input aax file provided."
 fi
 
 if [ ! -f "${SOURCE_DIR}/${AAX_FILE}" ]; then
@@ -25,45 +25,61 @@ if [ ! -f "${SOURCE_DIR}/${AAX_FILE}" ]; then
   die "The file can not be found."
 fi
 
-check1=`which ffmpeg`
-if [ "$check1" = "" ]
-then
-    die "ffmpeg is missing -> e.g.: apt-get install ffmpeg"
+check1=$(which ffmpeg)
+if [ "$check1" = "" ]; then
+  die "ffmpeg is missing -> e.g.: apt-get install ffmpeg"
 fi
 
-check1=`which ffprobe`
-if [ "$check1" = "" ]
-then
-    die "ffmpeg is missing -> e.g.: apt-get install ffmpeg"
+check1=$(which ffprobe)
+if [ "$check1" = "" ]; then
+  die "ffmpeg is missing -> e.g.: apt-get install ffmpeg"
 fi
 
-CHECKSUM=$(ffprobe "${SOURCE_DIR}/${AAX_FILE}" 2>&1 |grep checksum|sed 's/^.*== //'g)
+CHECKSUM=$(ffprobe "${SOURCE_DIR}/${AAX_FILE}" 2>&1 | grep checksum | sed 's/^.*== //'g)
 
 if [ -z "${CHECKSUM}" ]; then
-    die "No checksum found."
+  die "No checksum found."
 fi
 
 echo "Searching for activation bytes..."
 RCRACK=$(./rcrack . -h "${CHECKSUM}")
-ACTIVATION_BYTES=$(echo "${RCRACK}"|grep "hex:"|sed 's/^.*hex://g')
+ACTIVATION_BYTES=$(echo "${RCRACK}" | grep "hex:" | sed 's/^.*hex://g')
 echo "Your Activation bytes are: ${ACTIVATION_BYTES}"
 
 echo "Retrieving cover"
-ffmpeg -y -v quiet -i "${SOURCE_DIR}/${AAX_FILE}" "cover.png"
+ffmpeg -y -v quiet -i "${SOURCE_DIR}/${AAX_FILE}" "./cover.png"
+
+ARTIST="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.artist")"
+ALBUM_ARTIST="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.album_artist")"
+TITLE="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.title")"
+ALBUM="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.album")"
+YEAR="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.date")"
+GENRE="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.genre")"
+COMMENT="$(ffprobe -v quiet -print_format json -show_format "${SOURCE_DIR}/${AAX_FILE}" | jq -r ".format.tags.comment")"
+
+echo "Artist       : $ARTIST"
+echo "Album Artist : $ALBUM_ARTIST"
+echo "Title        : $TITLE"
+echo "Album        : $ALBUM"
+echo "Year         : $YEAR"
+echo "Genre        : $GENRE"
+echo "Comment      : $COMMENT"
 
 echo "Converting to audio..."
-set -x
-ffmpeg -activation_bytes "${ACTIVATION_BYTES}" -i "${SOURCE_DIR}/${AAX_FILE}" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -movflags use_metadata_tags "./temp.m4b"
-#ffmpeg -activation_bytes "${ACTIVATION_BYTES}" -i "${SOURCE_DIR}/${AAX_FILE}" -threads 4 -vn -y -acodec aac -strict -2 -map_metadata 0 -map_metadata:s:a 0:s:a -ac 1 "${OUTPUT_DIR}/${AAX_FILE%.*}.m4b"
-set +x
+ffmpeg -activation_bytes "${ACTIVATION_BYTES}" -i "${SOURCE_DIR}/${AAX_FILE}" -vn -c:a copy -map_metadata 0 -map_metadata:s:a 0:s:a -movflags use_metadata_tags "./temp.m4a"
+RETVAL=$?
+[ $RETVAL -eq 0 ] || die "The ffmpeg command failed with exit code: $RETVAL"
+
+AtomicParsley "./temp.m4a" --title "$TITLE" --album "$ALBUM" --artist "$ARTIST" --albumArtist "$ALBUM_ARTIST" --genre "$GENRE" --comment "$COMMENT" --year "$YEAR" --stik Audiobook --overWrite
+RETVAL=$?
+[ $RETVAL -eq 0 ] || die "The AtomicParsley command failed with exit code: $RETVAL"
 
 if [ -f "./cover.png" ]; then
-   mp4art --add "./cover.png" "./temp.m4b"
-   RETVAL=$?
-   [ $RETVAL -eq 0 ] || die "The mp4art command failed with exit code: $RETVAL"
+  mp4art --add "./cover.png" "./temp.m4b"
+  RETVAL=$?
+  [ $RETVAL -eq 0 ] || die "The mp4art command failed with exit code: $RETVAL"
 else
-   echo "No cover was found."
+  echo "No cover was found."
 fi
 
 mv -v ./temp.m4b "${OUTPUT_DIR}/${AAX_FILE%.*}.m4b"
-
